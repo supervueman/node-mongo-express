8c3cb0bb98cb09164f5bbb5ac535347f3434f64b
RinatDav <chaogen2@gmail.com>
2018-05-17 01:17:04 +0400

    Connect to database

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
+const app = require(`./app`);
+const database = require(`./database`);
 const config = require(`./config`);
 
-const app = express();
-
-app.set(`view engine`, `ejs`);
-app.use(bodyParser.urlencoded({extended: true}));
-
-const arr = [`hello`, `world`, `test`];
-
-app.get(`/`, (req, res) => res.render(`index`, {
-  arr: arr
-}));
-app.get(`/create`, (req, res) => res.render(`create`));
-app.post(`/create`, (req, res) => {
-  arr.push(req.body.text);
-  res.redirect(`/`);
-});
-
-app.listen(config.PORT, () => console.log(`Example app listening on port ${config.PORT}!`));
+database()
+  .then(info => {
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    app.listen(config.PORT, () => console.log(`Example app listening on port ${config.PORT}!`));
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
